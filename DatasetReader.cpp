951272a5d9108786b436ee4ab31a0b4ec7398223
worialhat <worialhat@gmail.com>
2024-10-31 00:06:18 +0300

    add DatasetReader class

---
+++ DatasetReader.cpp
@@ -0,0 +1,37 @@
+#include "DatasetReader.h"
+#include <iostream>
+
+DatasetReader::DatasetReader(string_view imagePath, string_view labelPath) {
+    mImage.open(imagePath.data(), std::ios::in | std::ios::binary);
+    mLabel.open(labelPath.data(), std::ios::in | std::ios::binary);
+    if (!mImage.is_open() || !mLabel.is_open()) {
+        throw std::runtime_error("Dataset is not found\n");
+    }
+
+    // skip headers
+    char number;
+    for (int i = 0; i < 16; ++i) {
+        mImage.read(&number, sizeof(char));
+    }
+    for (int i = 0; i < 8; ++i) {
+        mLabel.read(&number, sizeof(char));
+    }
+}
+
+DatasetReader::~DatasetReader() {
+    mImage.close();
+    mLabel.close();
+}
+
+void DatasetReader::ReadNext(std::array<double, IMG_SIZE> &data, int &expected) {
+    if (mImage.eof() || mLabel.eof()) {
+        throw std::runtime_error("EOF of dataset");
+    }
+    char tmp;
+    for (u_int32_t i = 0; i < IMG_SIZE; i++) {
+        mImage.read(&tmp, sizeof(char));
+        data[i] = tmp ? 1 : 0;
+    }
+    mLabel.read(&tmp, sizeof(char));
+    expected = tmp;
+}
---
+++ DatasetReader.h
@@ -0,0 +1,29 @@
+#ifndef PERCEPTRON_DATASETREADER_H
+#define PERCEPTRON_DATASETREADER_H
+
+#include <array>
+#include <cstdint>
+#include <fstream>
+#include <string_view>
+
+using std::string_view;
+
+class DatasetReader {
+    std::ifstream mImage;
+    std::ifstream mLabel;
+
+public:
+    // MNIST database config
+    static constexpr uint32_t IMG_WIDTH = 28;
+    static constexpr uint32_t IMG_HEIGHT = 28;
+    static constexpr uint32_t IMG_SIZE = IMG_WIDTH * IMG_HEIGHT;
+    static constexpr uint32_t SAMPLES_COUNT = 60000;
+
+    DatasetReader(string_view imagePath, string_view labelPath);
+    ~DatasetReader();
+
+    void ReadNext(std::array<double, IMG_SIZE> &data, int &expected);
+};
+
+
+#endif//PERCEPTRON_DATASETREADER_H
